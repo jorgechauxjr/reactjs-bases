4f8b86392715b57f1f7cde1076bea8f95b56530c
Jorge <chauxjorge@hotmail.com>
2021-07-06 16:42:38 -0500

    Add [PROPS] App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,21 @@ import './App.css';
 
 // this is not html but is similar it is JSX
 
-function Hello() {
+function Hello(props) {
+  console.log(props);
   return (
-    <div id="hi">HELLO WORLD!!</div>
+    <div id="hi">
+      <h3>{ props.subtitle }</h3>
+      { props.mytext }
+      </div>
   );
+  // I use . because props is received as an object
+  //ex: 
+  /*
+    {
+      mytext: "Hello Jhon"
+    }
+  */
 }
 
 // I can write the component App in 3 different ways
@@ -23,11 +34,17 @@ class App extends React.Component{
 // 2 as arrow function
 // const App = () => <div>This is my component: <Hello/></div>
 
-
+//3 as a normal function
+// Props
+// In App I call the component Hello and I pass a prop call mytext with value "Hello Jhon. Another props is subtitle with value lorem ipsum"
 function App() {
   return (
-  <div>This is my component: <Hello/></div>
+  <div>
+    This is my component:
+    <Hello mytext="Hello Jhon" subtitle="lorem ipsum"/>
+    <Hello mytext="Hola mundo" subtitle="Component 2"/>
+    <Hello mytext="Hello!" subtitle="Comp3"/>
+  </div>
   );
 }
-
 export default App;
