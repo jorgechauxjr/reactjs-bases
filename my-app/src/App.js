a116c07df3fb2a64ca0ee46b74f2a4b3e00f2721
Jorge <chauxjorge@hotmail.com>
2021-07-06 14:04:38 -0500

    Add [component] App in 3 different ways

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
 import './App.css';
+
+// this is not html but is similar it is JSX
+
+function Hello() {
+  return (
+    <div id="hi">HELLO WORLD!!</div>
+  );
+}
+
+// I can write the component App in 3 different ways
+// 1. as  class inheriting from React.component 2. as arrow function 3. as a normal function
+
+/* 1 as  class inheriting from React.component
+class App extends React.Component{
+  render() {
+    return <div>This is my component: <Hello/></div>
+  }
+}
+*/
+
+// 2 as arrow function
+// const App = () => <div>This is my component: <Hello/></div>
+
 
 function App() {
   return (
-    // this is not html but is similar it is JSX
-    <div>Hello world!</div>
+  <div>This is my component: <Hello/></div>
   );
 }
 
