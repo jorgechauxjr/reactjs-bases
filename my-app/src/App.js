90681f310a300b80e875dcb5d47a802738a09c04
Jorge <chauxjorge@hotmail.com>
2021-07-07 10:35:00 -0500

    Show tasks in App and modify tasks data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import './App.css';
+import tasks from './sample/tasksData.json'
 
+console.log(tasks)
+
+class App extends React.Component {
+  
+  state = {
+    tasksSt: tasks
+  }
+  
+  render() {
+    return <div>
+      { this.state.tasksSt.map(e => <p key={ e.id }>
+        { e.title } - { e.descripcion } - { e.done } - { e.id }
+        </p>) }
+    </div>
+  }
+}
 
 export default App;
