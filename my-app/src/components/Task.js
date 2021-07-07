ac6883edbbc3ed0982f614c4ebf89efa466ecd6c
Jorge <chauxjorge@hotmail.com>
2021-07-07 15:16:21 -0500

    Add 2 examples of style to component/Task.js: separate css and inline

--- my-app/src/components/Task.js
+++ my-app/src/components/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Task.css'
 
 // 3)
 // Receive every unique task from ./components/Tasks take each data and show it in a <div>
@@ -6,31 +7,43 @@ import React from 'react';
 
 class Task extends React.Component {
   render() {
-    return  <div>
-      {/* this.props.taskPr.title comes from ./components/Tasks */}
-      { this.props.taskPr.title } -
-      { this.props.taskPr.descripcion } -
-      { this.props.taskPr.done } -
-      { this.props.taskPr.id }
-    <input type="checkbox" />
-    <button>
-      x
-    </button>
-    </div>
-  }
+    const {taskPr} = this.props;
+    return  <p className="blue">
+      { taskPr.title } -
+      { taskPr.descripcion } -
+      { taskPr.done } -
+      { taskPr.id }
+      <input type="checkbox" />
+      <button style={btnDelete}>
+        x
+      </button>
+  </p>
+ }
 }
+
+// this is not css, it's JavaScript objects to give style
+const btnDelete = {
+  fontSize: '18px',
+  background: '#ea2027',
+  color: '#fff',
+  border: 'none',
+  padding: '10px 15px',
+  borderRadius: '50%',
+  cursor: 'pointer'
+}
+
 export default Task;
 
-// ANOTHER WAY O DOIN USING DESTRUCTURING JS
+// ANOTHER WAY OF DOING without destructuring
+
 // class Task extends React.Component {
 //   render() {
-//     const {taskPr} = this.props;
 //     return  <div>
 //       {/* this.props.taskPr.title comes from ./components/Tasks */}
-//       { taskPr.title } -
-//       { taskPr.descripcion } -
-//       { taskPr.done } -
-//       { taskPr.id }
+//       { this.props.taskPr.title } -
+//       { this.props.taskPr.descripcion } -
+//       { this.props.taskPr.done } -
+//       { this.props.taskPr.id }
 //     <input type="checkbox" />
 //     <button>
 //       x
