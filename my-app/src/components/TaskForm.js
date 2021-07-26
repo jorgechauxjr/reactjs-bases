fc5b730f4279746a6f57fe128de7c7092576fc66
Jorge <chauxjorge@hotmail.com>
2021-07-26 18:55:02 -0500

    Manage State Component TaskForm. Show in console the info received from user from inputs text

--- my-app/src/components/TaskForm.js
+++ my-app/src/components/TaskForm.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
 
 class TaskForm extends Component {
+
+  state = {
+    titleSt: '',
+    descriptionSt: ''
+  }
   
   submitAnswer = (event) => {
-    
+    console.log(this.state)
     event.preventDefault();
   }
 
   infoTyped = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.name, event.target.vaue)
+    
+    /* event.target.name es para saber desde que input se esta escribiendo. title o desciption
+      si usuario escribe en campo title,l evento devuelve en  name = title, si usuario escribe en la descripcion el evento devuelve en name= description */
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
   
   render() {
     return (
       <form onSubmit={this.submitAnswer}>
-        <input type="text" placeholder="Write a task" onChange={this.infoTyped} />
+        <input
+          type="text"
+          name="titleSt"
+          placeholder="Write a task"
+          onChange={this.infoTyped}
+          value={this.state.titleSt} />
         <br/>
         <br/>
-        <textarea placeholder="Write a task description" onChange={this.infoTyped}></textarea>
+        <textarea
+          name="descriptionSt"
+          placeholder="Write a task description"
+          onChange={this.infoTyped}
+          value={this.state.descriptionSt}> 
+        </textarea>
         <br/>
         <input type="submit"/>
       </form>
