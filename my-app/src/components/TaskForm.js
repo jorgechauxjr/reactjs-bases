ec78a1bff89134b572c78302bfe6e3b044705a3e
Jorge <chauxjorge@hotmail.com>
2021-07-26 18:23:12 -0500

    Add event onChange in TaskForm to capture the info when user write in text areas and show them in console

--- my-app/src/components/TaskForm.js
+++ my-app/src/components/TaskForm.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 
 class TaskForm extends Component {
   
-  submitAnswer = () => {
-    console.log("enviando...")
+  submitAnswer = (event) => {
+    
+    event.preventDefault();
+  }
+
+  infoTyped = (event) => {
+    console.log(event.target.value)
   }
   
   render() {
     return (
       <form onSubmit={this.submitAnswer}>
-        <input type="text" placeholder="Write a task" />
+        <input type="text" placeholder="Write a task" onChange={this.infoTyped} />
         <br/>
         <br/>
-        <textarea placeholder="Write a task description"></textarea>
+        <textarea placeholder="Write a task description" onChange={this.infoTyped}></textarea>
         <br/>
         <input type="submit"/>
       </form>
