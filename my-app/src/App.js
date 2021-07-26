0f19bf1792eb398864c15912fd1c8198e4f4c084
Jorge <chauxjorge@hotmail.com>
2021-07-26 18:07:13 -0500

    Organize App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,34 @@ import React from 'react';
 import './App.css';
 
 import tasks from './sample/tasksData.json'
+
+// Components
 import Tasks from './components/Tasks'
+import TaskForm from './components/TaskForm'
 
 console.log(tasks)
-// 1)
-// App have in the state different tasks
-// Send the data to the component Tasks
-//./components/Tasks
-
+/*1)
+ App have in the state different tasks
+ Send the data to the component Tasks
+./components/Tasks
+*/
 class App extends React.Component {
   
   state = {
     tasksSt: tasks
   }
-  // I call the component Tasks and pass it a property called tasksPr
-  // and give ti the property the value of the state tastSt
+  /* I call the component Tasks and pass it a property called tasksPr
+   and give ti the property the value of the state tastSt
 
-  // The property tasksPr is invoked in the /component/Tasks.js
-  // this.props.tasksPr
+   The property tasksPr is invoked in the /component/Tasks.js
+   this.props.tasksPr
+  */
   render() {
     return <div>
+      <TaskForm/>
       <Tasks tasksPr={this.state.tasksSt}/>
     </div>
+
   }
 }
-
 export default App;
